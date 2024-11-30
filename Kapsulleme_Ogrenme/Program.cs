d087231479771c43fc884a477d8115e2fde5943f
Pinaraltinkaya45 <189837954+Pinaraltinkaya45@users.noreply.github.com>
2024-11-30 17:47:29 +0300

    Add project files.

---
+++ Kapsulleme_Ogrenme/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapsulleme_Ogrenme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci1 = new Ogrenci();
+
+            ogrenci1.setIsim("Pınar");
+            string gelenisim = ogrenci1.getIsim();
+
+
+            if(gelenisim == "Pınar")
+            {
+                Console.WriteLine("Doğru Giriş Yaptınız");
+            }
+            else
+            {
+                Console.WriteLine("Hatalı Giriş Yaptınız");
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
